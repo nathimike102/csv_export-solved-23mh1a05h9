a06f607aa86b079685615bc085fa6d0c1785dc39
Nkosinathi Michael Sibanda <23mh1a05h9@acoe.edu.in>
2026-02-20 22:11:42 +0530

    feat: configure Express application

---
+++ src/app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const exports = require('./routes/exports');
+const logger = require('./utils/logger');
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Request logging
+app.use((req, res, next) => {
+  logger.info(`${req.method} ${req.path}`);
+  next();
+});
+
+// Health check endpoint
+app.get('/health', (req, res) => {
+  res.json({ status: 'ok' });
+});
+
+// Export routes
+app.use('/exports', exports);
+
+// 404 handler
+app.use((req, res) => {
+  res.status(404).json({
+    error: 'Not found'
+  });
+});
+
+// Error handler
+app.use((err, req, res, next) => {
+  logger.error('Unhandled error:', err);
+  res.status(500).json({
+    error: 'Internal server error'
+  });
+});
+
+module.exports = app;
